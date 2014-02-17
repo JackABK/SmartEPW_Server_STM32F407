37236dc7fb248b2db17edaab7e7853e8557f4f02
JackABK <abkabkabkyes@gmail.com>
2014-02-17 12:54:15 +0800

    add unit tests task to helpful debug

---
+++ Smart_EPW_lib/unit_tests.c
@@ -0,0 +1,51 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "unit_tests.h"
+#include "ultrasound.h"
+
+
+
+int tests_run_cnt = 0;
+
+int foo = 7;
+int bar = 4;
+
+static char * test_foo() {
+     assert("error, foo != 7", foo == 7);
+     return 0;
+}
+ 
+static char * test_bar() {
+     assert("error, bar != 5", bar == 5);
+     return 0;
+}
+
+static char * test_ultrasonic(){
+     assert("error, ultrasonic right non value", Get_CH1Distance() != 0);
+     return 0;
+}
+static char * all_tests() {
+     run_test(test_foo);
+     run_test(test_bar);     
+     run_test(test_ultrasonic);
+     return 0;
+}
+
+ 
+int unit_tests_task(void* p){
+     printf("=============TESTS UNIT STARTING=============\n");
+     char *result = all_tests();
+     if (result != 0) {
+         printf("%s\n", result);
+     }
+     else {
+         printf("=============ALL TESTS PASSED=============\n");
+     }
+     printf("Tests run: %d\n", tests_run_cnt);
+ 
+     return result != 0;
+}
+
+
+
+
---
+++ Smart_EPW_lib/unit_tests.h
@@ -0,0 +1,18 @@
+/*=============================================================================
+  *
+  * @file     : unit_tests.h
+  * @data       : 2014/2/17
+  * @brief   : unit_tests.c header file
+  *            the unit tests of method is refer to JTN002 - MinUnit -- a minimal unit testing framework for C,
+  *            very thanks to the original author, it's helpful me.
+  *            for info, see:  
+  *            http://www.jera.com/techinfo/jtns/jtn002.html 
+  *============================================================================*/
+#ifndef __UNIT_TESTS_H__
+#define __UNIT_TESTS_H__
+#define assert(message, test) do { if (!(test)) return message; } while (0)
+#define run_test(test) do { char *message = test(); tests_run_cnt++; \
+                                if (message) return message; } while (0)
+extern int tests_run_cnt; /*unit tests running count*/
+
+#endif /* __UNIT_TESTS_H__ */
