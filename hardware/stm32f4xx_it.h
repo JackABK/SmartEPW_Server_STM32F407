2dd8816194b5131ee244a527eb9f4505ca78c658
JackABK <abkabkabkyes@gmail.com>
2014-05-12 04:46:10 +0800

    remind the SysTick_Handler is already uesd for FreeRTOS

--- hardware/stm32f4xx_it.h
+++ hardware/stm32f4xx_it.h
@@ -32,7 +32,6 @@
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-
 void NMI_Handler(void);
 void HardFault_Handler(void);
 void MemManage_Handler(void);
@@ -41,7 +40,7 @@ void UsageFault_Handler(void);
 void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
-void SysTick_Handler(void);
+//void SysTick_Handler(void); /*in the FreeRTOS is already used for SysTick_Handler function.*/
 
 #ifdef __cplusplus
 }
